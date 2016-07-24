ae1680ca94753880d3368b0d71c70cf12244046c
vuvuzella <j.tabacii@gmail.com>
2016-07-24 14:00:51 +0800

    TODO: plot

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -28,8 +28,10 @@ emergeApp.config(['$routeProvider', 'uiGmapGoogleMapApiProvider',
 
 }]);
 
+// Socket.IO factory/service
 emergeApp.factory('mySocket', function(socketFactory) {
-  var myIoSocket = io.connect('http://localhost:3001');
+  var listenUrl = 'https://emerge-app.herokuapp.com/#/api/channel'
+  var myIoSocket = io.connect(listenUrl);
   var mySocket = socketFactory({
     ioSocket: myIoSocket
   });
@@ -37,33 +39,17 @@ emergeApp.factory('mySocket', function(socketFactory) {
   return mySocket;
 });
 
-// Wrap Socket.io into an Angular service
-// http://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/
-// emergeApp.factory('socket', function($rootScope) {
-//   var listenUrl = ('http://localhost:3001');
-//   var socket = io.connect(listenUrl);
-//   return {
-//     on: function(eventName, callback) {
-//       socket.on(eventName, function() {
-//         var args = arguments;
-//         $rootScope.$apply(function() {
-//           if (callback) {
-//             callback.apply(socket, args);
-//           }
-//         });
-//       });
-//     },
-//     emit: function(eventName, data, callback) {
-//       socket.emit(eventName, data, function() {
-//         var args = arguments;
-//         $rootScope.$apply(function() {
-//           if (callback) {
-//             callback.apply(socket, args);
-//           }
-//         });
-//       });
+// Google plotter factory/service
+// emergeApp.service('plot' ,function(markerCount, lat, lon) {
+//   // $scope.markerCount++;
+//   var marker = {
+//     idKey: markerCount,
+//     coord: {
+//       latitude: lat,
+//       longitude: lon
 //     }
 //   };
+//   return marker;
 // });
 
 
@@ -72,31 +58,34 @@ emergeApp.controller('MainController',
       '$scope',
       'uiGmapGoogleMapApi',
       'mySocket',
+      // 'plot',
     function($rootScope, $scope, uiGmapGoogleMapApi, mySocket) {
 
       uiGmapGoogleMapApi.then(function(maps) {
         // $scope.googleVersion = maps.version;
         // maps.visualRefresh = true;
       });
+
+      /** Markers **/
+      // $scope.markers = [];
+      // $scope.markerCount = 0;
+      // $scope.markers.push(plot(14, 122));
       
       $scope.map = {
         center: {
           latitude: 13,
           longitude: 122
         },
-        zoom: 1
+        zoom: 7
       };
 
-      $scope.sample = 'sample';
 
       /** Socket listeners to other servers **/
       mySocket.on('connection', function() {
-        // $scope.alert('connected...');
         console.log('connected...');
       });
 
       mySocket.on('message', function(data) {
-        // $scope.alert(data);
         console.log('message received');
         console.log('data is ' + data.hello);
 
